a11bbf4db092c821318c55999ce91c38cf7475bf
lmmc2 <lmmc2@cin.ufpe.br>
2018-04-02 09:16:54 -0300

    Funcao que retorna first de terminal implementada

--- 02-LL1parsing/src/br/ufpe/cin/if688/parsing/analysis/SetGenerator.java
+++ 02-LL1parsing/src/br/ufpe/cin/if688/parsing/analysis/SetGenerator.java
@@ -32,36 +32,40 @@ public final class SetGenerator {
     	
     }
     
-    public static Set<GeneralSymbol> firstOf(Grammar g, Nonterminal key, Set<GeneralSymbol> valor, Map<Nonterminal, Set<GeneralSymbol>> first){
-    	for (Production p: g.getProductions()) { //vejo producao por producao
-			if(p.getNonterminal().equals(key) ){ //analiso somente as producoes do nao terminal da vez
-				Iterator<GeneralSymbol> it = p.iterator();
-				while(it.hasNext()){ //lista de simbolos de uma producao do nao terminal q quero
-					GeneralSymbol s = it.next();
-					if(s instanceof Terminal){ //Se X é terminal FIRST(X) = {X} 
-						valor.add((Terminal) s);
-						break;
-					} else if(s.equals(SpecialSymbol.EPSILON)){ //Se X->ε, ε pertence a FIRST(X)
-						valor.add(SpecialSymbol.EPSILON);
-						break;
-					} else { //Se X-> Y1Y2...Yk, FIRST(Y1Y2...Yk) esta contido em FIRST(X)
-						Set<GeneralSymbol> valorS = first.get((Nonterminal) s);
-						valorS = firstOf(g, (Nonterminal) s, valorS, first);
-						if(!valorS.contains(SpecialSymbol.EPSILON)){ //FIRST(Y1) caso ε nao pertenca a FIRST(Y1)
-							valor.addAll(valorS);
-							break;
-						} else {
-							if(it.hasNext()){ //FIRST(Y1) - {ε} U FIRST(Y2...Yk) caso ε pertenca a FIRST(Y1)
-								valor.addAll(valorS);
-								valor.remove(SpecialSymbol.EPSILON);
-							} else { //Se ε pertence a FIRST(Yj) para todo j de 1 a k, ε pertence a FIRST(Y1Y2...Yk)
-								valor.addAll(valorS);
-							}
-						}						
+    public static Set<GeneralSymbol> firstOf(Grammar g, Symbol key, Set<GeneralSymbol> valor, Map<Nonterminal, Set<GeneralSymbol>> first){
+    	if(key.isTerminal()){
+    		valor.add((Terminal) key); //Se X é terminal FIRST(X) = {X} 
+    	} else {
+    		for (Production p: g.getProductions()) { //vejo producao por producao
+    			if(p.getNonterminal().equals(key) ){ //analiso somente as producoes do nao terminal da vez
+    				Iterator<GeneralSymbol> it = p.iterator();
+    				while(it.hasNext()){ //lista de simbolos de uma producao do nao terminal q quero
+    					GeneralSymbol s = it.next();
+    					if(s instanceof Terminal){  
+    						valor.add((Terminal) s);
+    						break;
+    					} else if(s.equals(SpecialSymbol.EPSILON)){ //Se X->ε, ε pertence a FIRST(X)
+    						valor.add(SpecialSymbol.EPSILON);
+    						break;
+    					} else { //Se X-> Y1Y2...Yk, FIRST(Y1Y2...Yk) esta contido em FIRST(X)
+    						Set<GeneralSymbol> valorS = first.get((Nonterminal) s);
+    						valorS = firstOf(g, (Nonterminal) s, valorS, first);
+    						if(!valorS.contains(SpecialSymbol.EPSILON)){ //FIRST(Y1) caso ε nao pertenca a FIRST(Y1)
+    							valor.addAll(valorS);
+								break;
+							} else {
+								if(it.hasNext()){ //FIRST(Y1) - {ε} U FIRST(Y2...Yk) caso ε pertenca a FIRST(Y1)
+									valor.addAll(valorS);
+									valor.remove(SpecialSymbol.EPSILON);
+								} else { //Se ε pertence a FIRST(Yj) para todo j de 1 a k, ε pertence a FIRST(Y1Y2...Yk)
+									valor.addAll(valorS);
+								}
+							}						
+						}
 					}
 				}
-			}
-        }
+	        }
+    	}
         return valor;
     }
 
@@ -75,7 +79,24 @@ public final class SetGenerator {
         
         /*
          * implemente aqui o método para retornar o conjunto follow
+         * $ ∈ FOLLOW(S), onde S é o símbolo inicial e $ é fim da entrada 
+         * Se existe uma produção A → αBβ, tudo que pertence a FIRST(β) exceto ε está em FOLLOW(B) 
+         * Se existe uma produção A → αB, então tudo que estiver em FOLLOW(A) estará em FOLLOW(B) 
+         * Se existe uma produção A → αBβ, e ε ∈ FIRST(β), tudo que estiver em FOLLOW(A) estará em FOLLOW(B) 
          */
+        for(Nonterminal key: follow.keySet()){ //calcular o follow de cada nao terminal
+        	Set<GeneralSymbol> valor = follow.get(key);
+        	if(key.equals((Nonterminal) g.getStartSymbol())){
+        		valor.add(SpecialSymbol.EOF); //$ ∈ FOLLOW(S), onde S é o símbolo inicial e $ é fim da entrada 
+        	}
+        	for(Production p: g.getProductions()){ 
+        		Iterator<GeneralSymbol> it = p.iterator();
+				while(it.hasNext()){ //vou ver todos os simbolos de tds producoes
+					GeneralSymbol s = it.next();
+					
+				}
+        	}
+        }
         
         return follow;
     }
