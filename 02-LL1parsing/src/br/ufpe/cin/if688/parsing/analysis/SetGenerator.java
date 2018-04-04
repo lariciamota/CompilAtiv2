8eb6f931edf4b946e365812026a7a33bb6dcb590
lmmc2 <lmmc2@cin.ufpe.br>
2018-04-04 16:22:09 -0300

    Table implementada

--- 02-LL1parsing/src/br/ufpe/cin/if688/parsing/analysis/SetGenerator.java
+++ 02-LL1parsing/src/br/ufpe/cin/if688/parsing/analysis/SetGenerator.java
@@ -11,10 +11,8 @@ public final class SetGenerator {
     	if (g == null) throw new NullPointerException("g nao pode ser nula.");
         
     	Map<Nonterminal, Set<GeneralSymbol>> first = initializeNonterminalMapping(g); 
-    	//aq tem um mapeamento de tds nao terminais com uma lista q vira a ser os firsts de cada nao terminal
     	
     	/*
-    	 * Implemente aqui o método para retornar o conjunto first
     	 * Se X é terminal FIRST(X) = {X} 
          * Se X->ε, ε pertence a FIRST(X)
          * Se X-> Y1Y2...Yk, FIRST(Y1Y2...Yk) esta contido em FIRST(X)
@@ -78,64 +76,59 @@ public final class SetGenerator {
         Map<Nonterminal, Set<GeneralSymbol>> follow = initializeNonterminalMapping(g);
         
         /*
-         * implemente aqui o método para retornar o conjunto follow
          * $ ∈ FOLLOW(S), onde S é o símbolo inicial e $ é fim da entrada 
          * Se existe uma produção A → αBβ, tudo que pertence a FIRST(β) exceto ε está em FOLLOW(B) 
          * Se existe uma produção A → αB, então tudo que estiver em FOLLOW(A) estará em FOLLOW(B) 
          * Se existe uma produção A → αBβ, e ε ∈ FIRST(β), tudo que estiver em FOLLOW(A) estará em FOLLOW(B) 
          */
-        for(Nonterminal key: follow.keySet()){ //calcular o follow de cada nao terminal
-    		Set<GeneralSymbol> valor = follow.get(key); 
-    		valor = followOf(g, key, valor, first, follow);
-    		
-    	}
+        follow.get(g.getStartSymbol()).add(SpecialSymbol.EOF); //$ ∈ FOLLOW(S), onde S é o símbolo inicial e $ é fim da entrada 
+        /*
+         * Para cada producao: criar um set auxiliar
+         * Adicionar a esse set o que tem no follow do nt da producao
+         * Ler a producao da direita p esquerda
+         * Para cada simbolo lido: 
+         * 		se for nao terminal adiciono o que tem no trailer
+         * 			se no first desse nao terminal tiver epsilon adiciono o que tem no first dele no trailer e excluo epsilon
+         * 			se nao tiver apago o que tem no trailer e adiciono o first dele
+         * 		se nao (sera terminal ou epsilon) apago o que tem no trailer e adiciono no trailer ele mesmo
+         */
+        boolean mudou = true;
+        while(mudou){
+        	mudou = false;
+        	for(Production p: g.getProductions()){ //analisar producao por producao
+        		Set<GeneralSymbol> aux = new HashSet<GeneralSymbol>();
+
+        		for(int i = p.getProduction().size() - 1; i >= 0; i--){ //ler os simbolos da producao da esquerda para a direita
+        			GeneralSymbol s = p.getProduction().get(i);
+        			if(i == p.getProduction().size() -1){
+            			aux.addAll(follow.get(p.getNonterminal()));
+        			}
+        			if(s instanceof Nonterminal){
+        				
+        				if(aux != null) {
+            				mudou = follow.get(s).addAll(aux);
+        				}
+        				Set<GeneralSymbol> fst = first.get(s);
+        				if(fst.contains(SpecialSymbol.EPSILON)){
+        					aux.addAll(fst);
+        					aux.remove(SpecialSymbol.EPSILON);
+        				} else {
+            				aux = new HashSet<GeneralSymbol>();
+            				aux.addAll(fst);
+        				}
+        			} else {
+        				aux = new HashSet<GeneralSymbol>();
+        				aux.add(s);
+        			}
+        			
+        		}
+        	}
+        	
+        }
         
         return follow;
     }
-    
-    public static Set<GeneralSymbol> followOf(Grammar g, Nonterminal key, Set<GeneralSymbol> valor, Map<Nonterminal, Set<GeneralSymbol>> first, Map<Nonterminal, Set<GeneralSymbol>> follow){
-    	if(key.equals((Nonterminal) g.getStartSymbol())){
-    		valor.add(SpecialSymbol.EOF); //$ ∈ FOLLOW(S), onde S é o símbolo inicial e $ é fim da entrada 
-    	}
-    	for(Production p: g.getProductions()){
-    		Iterator<GeneralSymbol> it = p.iterator();
-    		List<GeneralSymbol> l = p.getProduction();
-			while(it.hasNext()){ //vou ver todos os simbolos de tds producoes
-				GeneralSymbol s = it.next();
-				if(s.equals(key)){
-					if(it.hasNext()){ 
-						GeneralSymbol prox = l.get(l.indexOf(s) + 1); //proximo simbolo esta no indice seguinte ao s
-						Set<GeneralSymbol> firstBeta = first.get(prox);
-						if (firstBeta == null) { //para o caso do proximo ser terminal, first.get(prox) retornaria nulo, mas firstOf retorna o terminal
-							firstBeta = new HashSet<GeneralSymbol>();
-							firstBeta = firstOf(g, (Symbol)prox, firstBeta, first);
-						}
-						if(firstBeta.contains(SpecialSymbol.EPSILON)){
-							firstBeta.remove(SpecialSymbol.EPSILON);  
-							valor.addAll(firstBeta); //Se existe uma produção A → αBβ, tudo que pertence a FIRST(β) exceto ε está em FOLLOW(B)
-					        //Se existe uma produção A → αBβ, e ε ∈ FIRST(β), tudo que estiver em FOLLOW(A) estará em FOLLOW(B) 
-							Nonterminal A = p.getNonterminal();
-							if(!A.equals(key)){
-					    		Set<GeneralSymbol> valorA = follow.get(A); 
-								valor.addAll(followOf(g, A, valorA, first, follow));
-							}
-						} else {
-							valor.addAll(firstBeta); //Se existe uma produção A → αBβ, tudo que pertence a FIRST(β) exceto ε está em FOLLOW(B)
-						}
-					} else {
-				        //Se existe uma produção A → αB, então tudo que estiver em FOLLOW(A) estará em FOLLOW(B) 
-						Nonterminal A = p.getNonterminal();
-						if(!A.equals(key)){
-				    		Set<GeneralSymbol> valorA = follow.get(A); 
-							valor.addAll(followOf(g, A, valorA, first, follow));
-						}
-					}
-				}
-			}
-    	}
-    	return valor;
-    }
-    
+   
     
     //método para inicializar mapeamento nãoterminais -> conjunto de símbolos
     private static Map<Nonterminal, Set<GeneralSymbol>> initializeNonterminalMapping(Grammar g) {
@@ -147,4 +140,4 @@ public final class SetGenerator {
     return result;
 }
 
-} 
+}
--- 02-LL1parsing/src/br/ufpe/cin/if688/table/Table.java
+++ 02-LL1parsing/src/br/ufpe/cin/if688/table/Table.java
@@ -18,9 +18,67 @@ public final class Table {
         Map<LL1Key, List<GeneralSymbol>> parsingTable = new HashMap<LL1Key, List<GeneralSymbol>>();
 
         /*
-         * Implemente aqui o método para retornar a parsing table
+         * Para cada produção A → α de G
+         * Para todo a ∈ FIRST(α), adicione A → α em M[A,a]
+         * Se ε ∈ FIRST(α), então, para todo b ∈ FOLLOW(A), adicione A → α em M[A,b]
+         * a regra acima leva em conta também o símbolo $ 
          */
+       
+        for(Production p: g.getProductions()){
+			GeneralSymbol s = p.getProduction().get(0);
+			Nonterminal nt = p.getNonterminal();
+        	if(s instanceof Nonterminal){
+        		for(GeneralSymbol a: first.get(s)){
+        			if(a.equals(SpecialSymbol.EPSILON)){
+        				for(GeneralSymbol b: follow.get(s)){
+        					LL1Key key = searchTable(parsingTable, nt, b);
+        					if(key == null){
+        		        		parsingTable.put(new LL1Key(nt, b), new ArrayList<GeneralSymbol>());
+        		        		key = searchTable(parsingTable, nt, b);
+        					}
+                			parsingTable.get(key).addAll(p.getProduction()); //Se ε ∈ FIRST(α), então, para todo b ∈ FOLLOW(A), adicione A → α em M[A,b]
+        				}
+        			} else {
+    					LL1Key key = searchTable(parsingTable, nt, a);
+    					if(key == null){
+    		        		parsingTable.put(new LL1Key(nt, a), new ArrayList<GeneralSymbol>());
+    		        		key = searchTable(parsingTable, nt, a);
+    					}
+        				parsingTable.get(key).addAll(p.getProduction()); //Para todo a ∈ FIRST(α), adicione A → α em M[A,a]
+        			}
+
+        		}
+        	} else if(s instanceof Terminal){
+				LL1Key key = searchTable(parsingTable, nt, s);
+        		if(key == null){
+	        		parsingTable.put(new LL1Key(nt, s), new ArrayList<GeneralSymbol>());
+	        		key = searchTable(parsingTable, nt, s);
+        		}
+        		parsingTable.get(key).addAll(p.getProduction()); //Para todo a ∈ FIRST(α), adicione A → α em M[A,a]
+        	} else { //epsilon
+        		for(GeneralSymbol b: follow.get(nt)){
+					LL1Key key = searchTable(parsingTable, nt, b);
+					if(key == null){
+		        		parsingTable.put(new LL1Key(nt, b), new ArrayList<GeneralSymbol>());
+		        		key = searchTable(parsingTable, nt, b);
+					}
+        			parsingTable.get(key).addAll(p.getProduction()); //Se ε ∈ FIRST(α), então, para todo b ∈ FOLLOW(A), adicione A → α em M[A,b]
+				}
+        	}
+        }
         
         return parsingTable;
     }
+	public static LL1Key searchTable(Map<LL1Key, List<GeneralSymbol>> table, Nonterminal nonterminal, GeneralSymbol symbol) {
+		LL1Key key = new LL1Key(nonterminal, symbol);
+		
+		for(LL1Key tableKey : table.keySet()) {
+			if(key.equals(tableKey)) {
+				return tableKey;
+			}
+		}
+		
+		return null;
+	}
+
 }
